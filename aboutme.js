af65d89758bef0239187f1701fed5652b39600a7
ylimEuiL <ylimeuiliscool@gmail.com>
2016-09-26 14:09:23 -0600

    Add files via upload

--- aboutme.js
+++ aboutme.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	
+
 	$("#myinfo").hide();
-	
+
 	$("#aboutme").click(function() {
-		
 		$("#myinfo").fadeToggle(500);
 	});
+
+	// start of mouse over
+
+	$(".mousechange").mouseenter(function() {
+
+		$(this).css("font-size", "2.5em");
+		$(this).css("color", "#0000FF");
+
+	});
 	
+	$(".mousechange").mouseleave(function() {
+
+		$(this).css("font-size", "2em");
+		$(this).css("color", "#000000");
+
+	});
+
 });
